ea2722cd96dc40adbb9661b0fc44e2609ef723e9
shriyaGlo <shriya.goyal@globallogic.com>
2021-10-30 14:22:42 +0530

    server commit

--- fav-food-service/src/main/java/com/globallogic/favfoodservice/FavFoodServiceApplication.java
+++ fav-food-service/src/main/java/com/globallogic/favfoodservice/FavFoodServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,13 +13,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableEurekaClient
 public class FavFoodServiceApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(FavFoodServiceApplication.class, args);
 	}
 
 	@Bean
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectTimeout(3000);
+		clientHttpRequestFactory.setReadTimeout(3000);
+		return new RestTemplate(clientHttpRequestFactory);
 	}
 }
